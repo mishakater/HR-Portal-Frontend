24789d11948688c20190b0c0e851f37649823aea
Pavel Pshenyshniuk <39313584+Pshenya@users.noreply.github.com>
2020-03-31 18:15:53 +0300

    Add files via upload

---
+++ Pr1.py
@@ -0,0 +1,8 @@
+name = input("Введіть ваше ім'я ");
+birthday = input("Введіть дату вашого народження в форматі DD.MM.YYYY ");
+osvita = input("Де ви вчитеся? ");
+
+print("Ваше ім'я " + name);
+print("Ваша дата народження " + birthday);
+print("Ви вчитеся в " + osvita);
+
